aa99a535a56293aa5ab0f20a55866044af464c7c
lijie97 <lijie97@163.com>
2019-03-25 13:10:45 +0100

    gal FrameWork

---
+++ Assets/Script/Galgame/ScenesManager.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.IO;
+
+public class ScenesManager : MonoBehaviour {
+    int index, afs;
+    int maxAFS = 300;
+    public Text textObj;
+    public GameObject bgmObj;
+    public GameObject btnObj;
+    public Image backgroundObj;
+    public Image peopleObj;
+    string[] fileData;
+    private string newText;
+    // Use this for initialization
+    void Start () {
+        index = 0;
+        Debug.Log("start");
+        //GameObject btnObj = GameObject.Find("Canvas/Button");
+        Button btn = (Button)btnObj.GetComponent<Button>();
+        btn.onClick.AddListener(onClick);
+        readCSV();
+    }
+    void readCSV() {
+        string filePath = Application.streamingAssetsPath;
+        Debug.Log(filePath);
+        csvController.GetInstance().loadFile(filePath, "novel.csv ");
+        fileData = File.ReadAllLines(filePath);
+     
+    }
+    void onClick() {
+        //Debug.Log("On click");
+        index += 1;
+        if (afs > 0) afs = 0;
+        else afs = maxAFS;
+        newText = csvController.GetInstance().getString(index, 2);
+        Debug.Log(newText);
+    }
+	void Update () {
+        
+        if (afs > 0) afs--;
+	}
+}
---
+++ Assets/Script/Galgame/scvController.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.IO;
+using System.Collections.Generic;
+
+public class csvController
+{
+
+    static csvController csv;
+    public List<string[]> arrayData;
+
+    private csvController()   //单例，构造方法为私有
+    {
+        arrayData = new List<string[]>();
+    }
+
+    public static csvController GetInstance()   //单例方法获取对象
+    {
+        if (csv == null)
+        {
+            csv = new csvController();
+        }
+        return csv;
+    }
+
+    public void loadFile(string path, string fileName)
+    {
+        arrayData.Clear();
+        StreamReader sr = null;
+        try
+        {
+            string file_url = path + "/" + fileName;    //根据路径打开文件
+            sr = File.OpenText(file_url);
+            Debug.Log("File Find in " + file_url);
+        }
+        catch
+        {
+            Debug.Log("File cannot find ! ");
+            return;
+        }
+
+        string line;
+        while ((line = sr.ReadLine()) != null)   //按行读取
+        {
+            arrayData.Add(line.Split(','));   //每行逗号分隔,split()方法返回 string[]
+        }
+        sr.Close();
+        sr.Dispose();
+    }
+
+    public string getString(int row, int col)
+    {
+        return arrayData[row][col];
+    }
+    public int getInt(int row, int col)
+    {
+        return int.Parse(arrayData[row][col]);
+    }
+}
