5da593ac809eb59a9fae50994f4a9448e329b6ac
lijie97 <lijie97@163.com>
2019-04-07 14:17:17 +0200

    assemblage
    assemblage

---
+++ Assets/Script/StrategicTemp/managerData.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class managerData : MonoBehaviour {
+    Button classBtn, practiceBtn, talkBtn, restBtn;
+	// Use this for initialization
+	void Start () {
+        classBtn = GameObject.Find("Canvas/Buttons/ClassButton").GetComponent<Button>();
+        practiceBtn = GameObject.Find("Canvas/Buttons/PracticeButton").GetComponent<Button>();
+        talkBtn = GameObject.Find("Canvas/Buttons/TalkButton").GetComponent<Button>();
+        restBtn = GameObject.Find("Canvas/Buttons/ClassButton").GetComponent<Button>();
+
+    }
+    //
+
+
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
